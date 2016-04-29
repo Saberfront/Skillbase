36c464fa8d5be6730340c6fdf44d5292ec268787
WH <ACOKing@users.noreply.github.com>
2016-04-29 15:01:13 -0400

    Update PlayerController.js

--- controllers/PlayerController.js
+++ controllers/PlayerController.js
@@ -1,8 +1,10 @@
-SFApp.controller("PlayerController",function($scope,$firebaseAuth,$routeParams){
+SFApp.controller("PlayerController",function($scope,$firebaseAuth,$firebaseArray,$firebaseObject,$routeParams){
 		var ref = new Firebase("saberfront-skillbase.firebaseio.com");
 		var auth = $firebaseAuth(ref);
 		auth.$authWithOAuthPopup("google").then(function(authData) {
-        if(ref.child("Players") == null){
+				$scope.data = $firebaseObject(ref);
+        if($scope.data.child("Players") == null){
+        
         	console.log("Players list null");
         	
         }

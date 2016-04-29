945b261b91f45f4d103b0b7385bedd0630c61179
WH <ACOKing@users.noreply.github.com>
2016-04-29 12:02:13 -0400

    Create SkillsController.js

---
+++ controllers/SkillsController.js
@@ -0,0 +1,13 @@
+   SFApp.controller("SkillsController",function($scope){
+            $scope.SkillClasses = [
+                {
+                    name: "Ataru",
+                    description: "A form of lightsaber combat that can also utilize force attacks",
+                    combo: ["Force","Lightsaber"]
+                },
+                {
+                    name: "Force",
+                    
+                }
+                ];
+        });

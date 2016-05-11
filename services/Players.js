9eaa6e8a09a10016ab1312df5aeea3a724352dd4
WH <ACOKing@users.noreply.github.com>
2016-05-11 13:46:39 -0400

    Update Players.js

--- services/Players.js
+++ services/Players.js
@@ -1,6 +1,6 @@
 SFApp.factory("Players",function($firebaseArray){
 	var ref = new Firebase("saberfront-skillbase.firebaseio.com");
 		var Players = $firebaseArray(ref.child("Players"));
-		return Players.$ref();
+		return Players;
 
 });

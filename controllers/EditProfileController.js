c249d78bebf5d7e762685c653c7b4cf685090024
WH <ACOKing@users.noreply.github.com>
2016-05-16 11:22:44 -0400

    Update EditProfileController.js

--- controllers/EditProfileController.js
+++ controllers/EditProfileController.js
@@ -1,4 +1,15 @@
 SFApp.controller(function($scope,Players,AuthService){
+     	var auth = AuthService;
 
-
+  
+ auth.$onAuth(function(authData){
+ 		var obj =  Players.$getRecord(authData.uid);
+	$scope.dat = obj;
+	$scope.changeUserName = function(){
+	  $scope.dat.name = $scope.name;
+	  	Players[Players.$indexFor(authData.uid)] = $scope.dat;
+  	Players.$save(Players.$indexFor(authData.uid)).then(function(ref){
+  		ref.key() === authData.uid;
+  	});
+	};
 });

3c882650eada93796bcd20079af1b71880b20ebd
WH <ACOKing@users.noreply.github.com>
2016-05-17 13:17:29 -0400

    Update AdminController.js

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,3 +1,21 @@
 SFApp.controller('AdminController',function($scope,auth){
   $scope.auth = auth;
+  $('#menu-action').click(function() {
+  $('.sidebar').toggleClass('active');
+  $('.main').toggleClass('active');
+  $(this).toggleClass('active');
+
+  if ($('.sidebar').hasClass('active')) {
+    $(this).find('i').addClass('fa-close');
+    $(this).find('i').removeClass('fa-bars');
+  } else {
+    $(this).find('i').addClass('fa-bars');
+    $(this).find('i').removeClass('fa-close');
+  }
+});
+
+// Add hover feedback on menu
+$('#menu-action').hover(function() {
+    $('.sidebar').toggleClass('hovered');
+});
 });

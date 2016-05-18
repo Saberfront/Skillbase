98bca00ce7702e258a3299221fa82a3b90e1702e
WH <ACOKing@users.noreply.github.com>
2016-05-18 12:24:55 -0400

    Update PlayerController.js

--- controllers/PlayerController.js
+++ controllers/PlayerController.js
@@ -1,8 +1,5 @@
 SFApp.controller("PlayerController",function($scope,AuthService,Players,$firebaseArray,$routeParams){
-	if ($.mobile){
-		$.mobile.linkBindingEnabled = false;
-$.mobile.hashListeningEnabled = false;
-	}
+
 		var ref = new Firebase("saberfront-skillbase.firebaseio.com");
 		var auth = AuthService;
 		     	$scope.auth =  auth;

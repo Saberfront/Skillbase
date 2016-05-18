e7f36abdb7ffd2b8de52e99e3b5c772935fdeda8
WH <ACOKing@users.noreply.github.com>
2016-05-18 11:35:12 -0400

    Update HapticService.js

--- services/HapticService.js
+++ services/HapticService.js
@@ -1,6 +1,25 @@
-SFApp.factory("HapticService",function(){
-return {
+SFApp.factory("HapticService",function($rootScope){
+$rootScope.vibrateInterval = null;
+var HapticService = {
 
+StartVibrate: function(duration) {
+    window.navigator.vibrate(duration);
+},
 
+// Stops vibration
+ StopVibrate: function() {
+    // Clear interval and stop persistent vibrating
+    if( $rootScope.vibrateInterval) $interval.cancel($rootScope.vibrateInterval);
+    window.navigator.vibrate(0);
+},
+
+// Start persistent vibration at given duration and interval
+// Assumes a number value is given
+StartPeristentVibrate: function(duration, interval) {
+    $rootScope.vibrateInterval = $interval(function() {
+        HapticService.StartVibrate(duration);
+    }, interval);
+}
 };
+return HapticService
 });

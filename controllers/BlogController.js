e67f073fdf29bb39f44ed913d8b36c73bd47664b
WH <ACOKing@users.noreply.github.com>
2016-05-20 14:07:08 -0400

    Update BlogController.js

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -22,12 +22,8 @@ SFApp.controller('BlogController', function($rootScope,$scope,Players,AuthServic
 
   $scope.tinymceOptions = { 
 
-  plugins: [
-    'advlist autolink lists link image charmap print preview anchor',
-    'searchreplace visualblocks code fullscreen',
-    'insertdatetime media table contextmenu paste code'
-  ],
-  toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+  plugins:  'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste code',
+  toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
 };
   
     $scope.blog.title = "Saberfront Skillbase Blog";

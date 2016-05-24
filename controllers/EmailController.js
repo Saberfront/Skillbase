7820877447c2c40234b1172b24d89d9cdf875762
WH <ACOKing@users.noreply.github.com>
2016-05-24 12:21:25 -0400

    Update EmailController.js

--- controllers/EmailController.js
+++ controllers/EmailController.js
@@ -3,23 +3,28 @@ SFApp.controller("EmailController",function($scope){
      {
       sender: "Sarach Connor",
       date: "Jul 15",
-      message: "<strong>I've been hunted - </strong>A crazing robot ..."
+      name: "I've been hunted",
+      text: "A crazing robot ..."
      },
      {
         sender: "Jeremy Clarkson",
         date: "Jul 15",
-        message: "The brand new season of Top Gear"
+        name: "No subject",
+        text: "The brand new season of Top Gear"
      },
      {
         sender: "Eureka.com",
         date: "Jul 14",
-        message: "Interface design - </strong>Hi Greg ...<"
+        message: "Interface design"
+        text: "Hi Greg ..."
         
      },
      {
         sender: "Jeremy Legrand ",
         date: "Jul 13",
-        message: "CSS Responsive - </strong>Here is my hack to ..."
+        message: "CSS Responsive",
+        text: "Here is my hack to ..."
+
      }
    ];
 

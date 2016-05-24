deb9587016cd0bee7879cc61f5da35d3e9172a2e
WH <ACOKing@users.noreply.github.com>
2016-05-24 12:05:32 -0400

    Create EmailController.js

---
+++ controllers/EmailController.js
@@ -0,0 +1,10 @@
+SFApp.controller("EmailController",function($scope){
+   $scope.messages = [
+     {
+      sender: "Sarach Connor",
+      date: "Jul 15",
+      message: "<strong>I've been hunted - </strong>A crazing robot ..."
+     }
+   ];
+
+});

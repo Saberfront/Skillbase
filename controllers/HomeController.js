3dcf60c2c0ea9cc8ccf731aeae639736731beb9f
WH <ACOKing@users.noreply.github.com>
2016-07-29 19:14:18 -0400

    Update HomeController.js

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -3,7 +3,9 @@ SFApp.Controller("HomeController",function($scope){
 
 if (window.DeviceMotionEvent) {
    $scope.gameLink = "robloxmobile://placeID=330893648";
+$scope.platform = "Mobile";
 } else {
    $scope.gameLink = "";
+  $scope.platform = 'for PC';
 }
 });

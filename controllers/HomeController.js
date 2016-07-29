136d1ce5aa3b84aca985abb5b1e10af0ba9571f7
WH <ACOKing@users.noreply.github.com>
2016-07-29 19:31:05 -0400

    Update HomeController.js

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -2,8 +2,6 @@ SFApp.controller("HomeController",function($scope,$sce){
 
 
 if (window.DeviceMotionEvent) {
-    $sce.resourceUrlWhitelist("robloxmobile://placeID=330893648");
-   $scope.gameLink = "robloxmobile://placeID=330893648";
 $scope.platform = "Mobile";
 } else {
    $scope.gameLink = "";

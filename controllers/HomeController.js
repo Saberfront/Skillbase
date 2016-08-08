ca1d23888cde01e388bf1fcae5442c50cf0ce1b0
WH <ACOKing@users.noreply.github.com>
2016-08-08 17:17:30 -0400

    Update HomeController.js

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,8 +1,8 @@
 SFApp.controller("HomeController",function($scope,$sce){
 function detectmob() { 
- if( navigator.userAgent.match(/Android/i)
+ if( window.navigator.userAgent.match(/Android/i)
  || navigator.userAgent.match(/webOS/i)
- || navigator.userAgent.match(/iPhone/i)
+ || window.navigator.userAgent.match(/iPhone/i)
  || navigator.userAgent.match(/iPad/i)
  || navigator.userAgent.match(/iPod/i)
  || navigator.userAgent.match(/BlackBerry/i)

81af04d0112d6cc2920b4e7324c040ddc70b76ec
WH <ACOKing@users.noreply.github.com>
2016-08-08 17:15:26 -0400

    Update HomeController.js

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,5 +1,19 @@
 SFApp.controller("HomeController",function($scope,$sce){
-
-$scope.mobile = (window.orientationevent != null);
+function detectmob() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+$scope.mobile = detection();
 console.log($scope.mobile)
 });

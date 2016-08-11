5260de96181b354f6158783a1682662ad036a30f
WH <ACOKing@users.noreply.github.com>
2016-08-11 16:35:25 -0400

    Update sw.js

--- controllers/sw.js
+++ controllers/sw.js
@@ -2,6 +2,11 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
  
-  caches.open('saberfront-skillbase').then(function(cache) {})
+  caches.open('saberfront-skillbase').then(function(cache) {
+
+ return cache.addAll([
+       '/',
+       '/index.html']);
+})
 );
 });
